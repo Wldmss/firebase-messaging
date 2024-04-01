71be3cd81ca028a9e12e5a6469d1586bfb2d36c0
Wldmss <tmqpsoi@gmail.com>
2024-04-01 13:21:09 +0900

    first commit

---
+++ src/main/java/com/example/firebasemessaging/FirebaseMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.example.firebasemessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirebaseMessagingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firebasemessaging/FirebaseMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firebasemessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseMessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
